a1e1c7255ec4e3c295797c14cd82756352525f56
wang.shiyu3 <wang.shiyu3@husky.neu.edu>
2019-03-19 21:56:56 -0400

    cloudwatch1.6

--- inline.sh
+++ inline.sh
@@ -1,20 +1,20 @@
-sudo yum update -y,
-sudo yum install -y epel-release gcc-c++,
-curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -,
-sudo yum install -y nodejs,
-sudo yum install -y ruby,
-sudo yum install -y wget,
-cd /home/centos,
-wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install,
-chmod +x ./install,
-sudo ./install auto,
-sudo service codedeploy-agent start,
-rm ./install,
-wget https://s3.amazonaws.com/amazoncloudwatch-agent/centos/amd64/latest/amazon-cloudwatch-agent.rpm,
-wget https://s3.amazonaws.com/amazoncloudwatch-agent/assets/amazon-cloudwatch-agent.gpg,
-gpg --import amazon-cloudwatch-agent.gpg,
-gpg --fingerprint 3B789C72,
-wget https://s3.amazonaws.com/amazoncloudwatch-agent/centos/amd64/latest/amazon-cloudwatch-agent.rpm.sig,
-gpg --verify amazon-cloudwatch-agent.rpm.sig amazon-cloudwatch-agent.rpm,
-sudo rpm -U ./amazon-cloudwatch-agent.rpm,
+sudo yum update -y
+sudo yum install -y epel-release gcc-c++
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+sudo yum install -y nodejs
+sudo yum install -y ruby
+sudo yum install -y wget
+cd /home/centos
+wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+sudo service codedeploy-agent start
+rm ./install
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/centos/amd64/latest/amazon-cloudwatch-agent.rpm
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/assets/amazon-cloudwatch-agent.gpg
+gpg --import amazon-cloudwatch-agent.gpg
+gpg --fingerprint 3B789C72
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/centos/amd64/latest/amazon-cloudwatch-agent.rpm.sig
+gpg --verify amazon-cloudwatch-agent.rpm.sig amazon-cloudwatch-agent.rpm
+sudo rpm -U ./amazon-cloudwatch-agent.rpm
 sudo touch /etc/systemd/system/cloudwatch.service
